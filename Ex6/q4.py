6a7133dd4476999ef16efb6d4106dab6ab291182
avieha <aviemhadar@gmail.com>
2022-12-05 15:55:27 +0200

    check_cycle fixed

--- Ex6/q4.py
+++ Ex6/q4.py
@@ -10,57 +10,60 @@ def check_cycle(g):
     """
     :param g: Directed graph
     :return: if there is a DiGraph with weights Mul < 1
+    >>> # Cycle: [(2, 7, 'forward'), (7, 3, 'forward'), (3, 2, 'forward')]
+    Mul: 15
+    Cycle: [(5, 8, 'forward'), (8, 4, 'forward'), (4, 5, 'forward')]
+    Mul: 42
     >>> g = nx.DiGraph()
     >>> edges = [(6, 1, 10), (1, 2, 2), (2, 7, 1), (3, 2, 3), (7, 3, 5), (5, 8, 6), (8, 4, 7), (4, 5, 1)]
     >>> g.add_weighted_edges_from(edges)
     >>> print(check_cycle(g))
-    Cycle: [(2, 7, 'forward'), (7, 3, 'forward'), (3, 2, 'forward')]
-    Mul: 15
-    Cycle: [(5, 8, 'forward'), (8, 4, 'forward'), (4, 5, 'forward')]
-    Mul: 42
     no cycle with Mul<1 found
     >>> # 2 cycles in the graph, first with Mul 6 and the second < 1
     >>> g = nx.DiGraph()
     >>> edges = [(6, 1, 0.1), (1, 2, 0.2), (2, 7, 10), (3, 2, 1.3), (7, 3, 0.5), (5, 8, 0.6), (8, 4, 0.7), (4, 5, 1.5)]
     >>> g.add_weighted_edges_from(edges)
     >>> print(check_cycle(g))
-    Cycle: [(2, 7, 'forward'), (7, 3, 'forward'), (3, 2, 'forward')]
-    Mul: 6.5
-    Cycle: [(5, 8, 'forward'), (8, 4, 'forward'), (4, 5, 'forward')]
-    Mul: 0.63
-    ([(5, 8, 'forward'), (8, 4, 'forward'), (4, 5, 'forward')], 0.63)
+    Cycle:8->4->5
+    Mul:0.6299999999999999
     >>> g = nx.DiGraph()
     >>> edges = [(1, 2, 0.5), (2, 1, 0.5)]
     >>> g.add_weighted_edges_from(edges)
     >>> print(check_cycle(g))
-    Cycle: [(1, 2, 'forward'), (2, 1, 'forward')]
-    Mul: 0.25
-    ([(1, 2, 'forward'), (2, 1, 'forward')], 0.25)
+    Cycle:1->2
+    Mul:0.25
+    >>> # Cycle: [(1, 2, 'forward'), (2, 1, 'forward')]
+        Mul: 1
     >>> g = nx.DiGraph()
     >>> edges = [(1, 2, 1), (2, 1,1)]
     >>> g.add_weighted_edges_from(edges)
     >>> print(check_cycle(g))
-    Cycle: [(1, 2, 'forward'), (2, 1, 'forward')]
-    Mul: 1
     no cycle with Mul<1 found
+    >>> g = nx.DiGraph()
+    >>> edges = [(1, 2, 2.5), (2, 3, 2), (3, 1, 1), (3, 4, 0.5), (4, 2, 0.1)]
+    >>> g.add_weighted_edges_from(edges)
+    >>> print(check_cycle(g))
+    Cycle:2->3->4
+    Mul:0.1
     """
-    try:
-        while 1:
-            found_cycle = nx.find_cycle(g, orientation="original")
-            print("Cycle:", found_cycle)
-            Mul = 1
-            for edge in found_cycle:
-                u = edge[0]
-                v = edge[1]
-                weight = g[u][v]["weight"]
-                Mul *= weight
-            print("Mul:", Mul)
-            if Mul < 1:
-                return found_cycle, Mul
-            for arg in found_cycle:
-                g.remove_edge(arg[0], arg[1])
-    except:
-        return "no cycle with Mul<1 found"
+    found_cycle = nx.simple_cycles(g)
+    for cycle in found_cycle:
+        Mul = 1
+        res = ""
+        for i in range(len(cycle) - 1):
+            res += str(cycle[i]) + "->"
+            u = cycle[i]
+            v = cycle[i + 1]
+            weight = g[u][v]["weight"]
+            Mul *= weight
+        u = cycle[len(cycle) - 1]
+        v = cycle[0]
+        weight = g[u][v]["weight"]
+        Mul *= weight
+        res += str(u)
+        if Mul < 1:
+            return "Cycle:" + res + "\nMul:" + str(Mul)
+    return "no cycle with Mul<1 found"
 
 
 def check_dijkstra(g, ver_num):
